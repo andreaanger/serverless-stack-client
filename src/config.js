770dbb5644e90f5e353c64a1bd123c786c71ef7f
Andrea Anger <a.anger.tech@gmail.com>
2020-04-17 14:38:43 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "andrea-notes-app"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://1tyc7mypb5.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_KipLcLIs9",
+      APP_CLIENT_ID: "17fnttb8k2c1e7knliqo6icldj",
+      IDENTITY_POOL_ID: "us-east-1:8b5ce5f0-2639-413c-9c59-9a0cc586bba0"
+    }
+  };
