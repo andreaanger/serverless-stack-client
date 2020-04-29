cd25700965322e692573f522db9d6b881cf20f5d
Andrea Anger <a.anger.tech@gmail.com>
2020-04-29 12:04:33 -0700

    Splitting into environments

--- src/config.js
+++ src/config.js
@@ -1,18 +1,46 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   STRIPE_KEY: "pk_test_Tt0HxUoTrPxH47IlVvAFMMZa00Y47f3IVp",
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "andrea-notes-app"
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://1tyc7mypb5.execute-api.us-east-1.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_KipLcLIs9",
-      APP_CLIENT_ID: "17fnttb8k2c1e7knliqo6icldj",
-      IDENTITY_POOL_ID: "us-east-1:8b5ce5f0-2639-413c-9c59-9a0cc586bba0"
-    }
-  };
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1l0qd0f3rcfdn"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://api.andreatests.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ahW6WYV85",
+    APP_CLIENT_ID: "34ng7o80t7dh82mochft9gcu4g",
+    IDENTITY_POOL_ID: "us-east-1:3dc5cacb-7771-4d8a-b32e-56d38d22ae14"
+  }
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_Tt0HxUoTrPxH47IlVvAFMMZa00Y47f3IVp",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-1uk2p7c08so7j"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://api.andreatests.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_hFzCHNi1x",
+    APP_CLIENT_ID: "2s2v2gqvgpcf5le6j9rhmsvc8j",
+    IDENTITY_POOL_ID: "us-east-1:a0998aa3-0195-4432-ab78-3f05dba89685"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
+};
